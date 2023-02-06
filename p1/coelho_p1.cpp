f216f0eb0079d4e5c1e95f29203fe11477f69bc1
Tara Coelho <Tarascomputer@Taras-MacBook-Air.local>
2023-02-06 17:14:46 -0500

    initial commit

---
+++ p1/coelho_p1.cpp
@@ -0,0 +1,11 @@
+#include "coelho_p1.hpp"
+
+using namespace std;
+
+turing_machine::turing_machine(){
+    cout << "turing machine created" << endl;
+}
+
+int main(){
+    turing_machine test;
+}
---
+++ p1/coelho_p1.hpp
@@ -0,0 +1,16 @@
+#ifndef COELHO_P1_HPP
+#define COELHO_P1_HPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+class turing_machine{
+    public:
+        turing_machine();
+    private:
+};
+
+#endif
