cf51515c01959c79f64b8eba5338c2535b82331b
Tara Coelho <Tarascomputer@Taras-MacBook-Air.local>
2023-02-07 14:00:12 -0500

    transition capability added

--- p1/coelho_p1.cpp
+++ p1/coelho_p1.cpp
@@ -2,15 +2,41 @@
 
 using namespace std;
 
+struct transition{
+    int q,r;
+    char a, b, x;
+
+    char get_a(){
+        return a;
+    }
+};
+
 struct state{
     int state_num;
     string action;
+    vector<transition> curr_state_transitions;
+
+    bool accepts(char symbol){
+        bool flag = false;
+        for(int i = 0; i < curr_state_transitions.size(); i++){
+            if(curr_state_transitions.at(i).get_a() == symbol){
+                flag = true;
+            }
+        }
+        return flag;
+    }
+
+    transition get_transition(char symbol){
+        transition trans;
+        for(int i = 0; i < curr_state_transitions.size(); i++){
+            if(curr_state_transitions.at(i).get_a() == symbol){
+                trans = curr_state_transitions.at(i);
+            }
+        }
+        return trans;
+    }
 };
 
-struct transition{
-    int q,r;
-    string a, b, x;
-};
 
 turing_machine::turing_machine(){
     //empty constructor
@@ -20,13 +46,7 @@ turing_machine::turing_machine(string file, char* input, int num_transitions){
     read_input(file);
     this->input = input;
     this->num_transitions = num_transitions;
-
-    /* cout << "reading turing machine input from " << file << endl;
-    cout << "input string " << endl;
-    for(int i = 0; i < 5; i++){
-        cout << input[i] << endl;
-    }
-    cout << "max transitions " << num_transitions << endl; */
+    execute();
 }
 
 void turing_machine::read_input(string filename){
@@ -51,7 +71,7 @@ void turing_machine::read_input(string filename){
         }
         if(behavior == "transition"){
             int q, r;
-            string a,b,x;
+            char a,b,x;
 
             s >> q >> a >> r >> b >> x;
 
@@ -64,6 +84,8 @@ void turing_machine::read_input(string filename){
 
             transitions.push_back(new_transition);
 
+            states.at(q).curr_state_transitions.push_back(new_transition);
+
         }
 
     }
@@ -71,6 +93,30 @@ void turing_machine::read_input(string filename){
 
 string turing_machine::execute(){
     string result = "";
+    vector<char> tape;
+    int tape_head_index, curr_state_num = 0;
+    state curr_state = states.at(0);
+
+    for(int i = 0; i < strlen(input); i++){
+        tape.push_back(input[i]);
+    }
+
+    if(curr_state.accepts(tape.at(tape_head_index)) == true){
+        curr_state_num = curr_state.get_transition(tape.at(tape_head_index)).r;
+        curr_state = states.at(curr_state_num);
+        cout << "new state = " << curr_state.state_num << endl;
+    }
+
+    //printing all states and their associated transitions
+    /* for(int n = 0; n < states.size(); n++){
+        state curr_state = states.at(n);
+        cout << "state " << states.at(n).state_num << endl;
+
+        for(int i = 0; i < curr_state.curr_state_transitions.size(); i++){
+            cout << curr_state.curr_state_transitions.at(i).q << " -> " << curr_state.curr_state_transitions.at(i).r << " when received " << curr_state.curr_state_transitions.at(i).a << endl;
+        }
+    } */
+
     return result;
 }
 
