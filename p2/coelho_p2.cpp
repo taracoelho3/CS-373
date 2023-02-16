9009b6620e17da7b7117576f0182f135548f951d
Tara Coelho <Tarascomputer@Taras-MacBook-Air.local>
2023-02-16 16:34:54 -0500

    skeleton

---
+++ p2/coelho_p2.cpp
@@ -0,0 +1,13 @@
+#include "coelho_p2.hpp"
+
+using namespace std;
+
+
+int main(int argc, char *argv[]){
+    if(argc != 4){
+        cout << "invalid input" << endl;
+    }
+    else{
+       
+    }
+}
---
+++ p2/coelho_p2.hpp
@@ -0,0 +1,17 @@
+#ifndef COELHO_P1_HPP
+#define COELHO_P1_HPP
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <ostream>
+#include <string>
+
+using namespace std;
+
+
+
+#endif
